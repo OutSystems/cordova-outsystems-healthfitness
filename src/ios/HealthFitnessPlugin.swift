51cbf87a1e8fb85b598a674556b063d903df5db7
Carlos Corrêa <carlscorrea@gmail.com>
2021-09-07 21:31:27 +0100

    pluginInitialize implemented

---
+++ src/ios/HealthFitnessPlugin.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+
+class HealthFitnessPlugin {
+
+    func requestPermissions() {
+        
+    }
+    
+    
+}
--- src/ios/OSHealthFitness.swift
+++ src/ios/OSHealthFitness.swift
@@ -1,11 +1,40 @@
 import Foundation
+import UIKit
 
 @objc(OSHealthFitness)
 class OSHealthFitness: CDVPlugin {
-
-    @objc(initialize:)
-    func initialize(command: CDVInvokedUrlCommand) {
-        
+    var plugin: HealthFitnessPlugin?
+    var callbackId:String=""
+    
+    override func pluginInitialize() {
+        plugin = HealthFitnessPlugin()
     }
-
+    
+    
+    @objc(requestPermissions:)
+    func requestPermissions(command: CDVInvokedUrlCommand) {
+        callbackId = command.callbackId
+    
+        
+        do {
+            try plugin?.requestPermissions()
+        } catch {
+            sendResult(result: "", error: error.localizedDescription)
+        }
+    }
+    
+    @objc(sendResult:error:)
+        func sendResult(result:String,error:String) {
+            var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR);
+            
+            if error.isEmpty {
+                let resultArray = [result]
+                pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: resultArray)
+            } else {
+                let errorDict = ["code": "0", "message": error]
+                pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: errorDict);
+            }
+            self.commandDelegate!.send(pluginResult, callbackId: callbackId);
+        }
+    
 }
