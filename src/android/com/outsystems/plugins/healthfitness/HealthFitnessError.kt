8dac0dae609635693c20d25d9a881eee29327dc7
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2021-09-21 11:44:57 +0100

    refactor: change error message

--- src/android/com/outsystems/plugins/healthfitness/HealthFitnessError.kt
+++ src/android/com/outsystems/plugins/healthfitness/HealthFitnessError.kt
@@ -2,7 +2,7 @@ package com.outsystems.plugins.healthfitness
 
 enum class HealthFitnessError(val code: Int, val message: String) {
 
-    GOOGLE_SERVICES_ERROR_RESOLVABLE (100, "Google Play Services error user resolvable"),
+    GOOGLE_SERVICES_ERROR_RESOLVABLE (100, "It looks like Google Play services aren't available on your device"),
     GOOGLE_SERVICES_ERROR (101, "Google Play Services error"),
     WRITE_VALUE_INCORRECT_FORMAT_ERROR(102, "Value provided is not in correct format"),
     WRITE_VALUE_OUT_OF_RANGE_ERROR(103, "Value provided is out of range for this variable"),
