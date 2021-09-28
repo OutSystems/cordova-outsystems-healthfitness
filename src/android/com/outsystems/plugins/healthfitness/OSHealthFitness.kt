9a23e9fc096d29503e4428f1fa2c64548b8ee3a5
Nelson Lopes Silva <5671236+nflsilva@users.noreply.github.com>
2021-09-28 18:27:33 +0100

    Added fix for old APIs that do not cointain ACTIVITY_RECOGNITION

--- src/android/com/outsystems/plugins/healthfitness/OSHealthFitness.kt
+++ src/android/com/outsystems/plugins/healthfitness/OSHealthFitness.kt
@@ -4,9 +4,13 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Build.VERSION.SDK_INT
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.api.Api
 import com.google.gson.Gson
 import com.outsystems.plugins.healthfitness.store.AdvancedQueryParameters
 import com.outsystems.plugins.healthfitness.store.HealthStore
@@ -82,26 +86,24 @@ class OSHealthFitness : CordovaImplementation() {
         checkAndGrantPermissions()
     }
 
-    private fun areAndroidPermissionsGranted(permissions: Array<String>): Boolean {
+    private fun areAndroidPermissionsGranted(permissions: List<String>): Boolean {
         permissions.forEach {
-            if (ContextCompat.checkSelfPermission(
-                    cordova.activity,
-                    it
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
+            if (ContextCompat.checkSelfPermission(cordova.activity, it) != PackageManager.PERMISSION_GRANTED) {
                 return false
             }
         }
         return true
     }
 
-
     private fun checkAndGrantPermissions(){
-        val permissions = arrayOf(
-            Manifest.permission.ACTIVITY_RECOGNITION,
+        val permissions = mutableListOf(
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.BODY_SENSORS
         )
+
+        if(SDK_INT >= Build.VERSION_CODES.Q) {
+            permissions.add(Manifest.permission.ACTIVITY_RECOGNITION)
+        }
 
         if (areAndroidPermissionsGranted(permissions)) {
             if(!healthStore!!.areGoogleFitPermissionsGranted()){
@@ -113,7 +115,7 @@ class OSHealthFitness : CordovaImplementation() {
             PermissionHelper.requestPermissions(
                 this,
                 ACTIVITY_LOCATION_PERMISSIONS_REQUEST_CODE,
-                permissions
+                permissions.toTypedArray()
             )
         }
     }
