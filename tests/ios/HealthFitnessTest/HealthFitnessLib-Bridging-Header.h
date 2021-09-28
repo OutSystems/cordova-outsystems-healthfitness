37614f89e4f552e4ab4b6f5ed5dc015b55cdb421
Carlos Corrêa <carlscorrea@gmail.com>
2021-09-28 18:17:48 +0100

    wip: unit tests

---
+++ tests/ios/HealthFitnessTest/HealthFitnessLib-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OSFileViewerTests.
+FOUNDATION_EXPORT double OSFileViewerTestsVersionNumber;
+
+//! Project version string for OSFileViewerTests.
+FOUNDATION_EXPORT const unsigned char OSFileViewerTestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSFileViewerTests/PublicHeader.h>
---
+++ tests/ios/HealthFitnessTest/HealthFitnessLibTests/OSHealthFitnessTests.swift
@@ -0,0 +1,19 @@
+//
+//  OSHealthFitnessTests.swift
+//  HealthFitnessLibTests
+//
+//  Created by Carlos Correa on 28/09/2021.
+//
+
+import XCTest
+@testable import HealthFitnessLib
+
+class OSHealthFitnessTests: XCTestCase {
+    
+    func test_When_emptyUrlPassed_Expect_invalidEmptyURL() throws {
+        
+    }
+    
+   
+
+}
