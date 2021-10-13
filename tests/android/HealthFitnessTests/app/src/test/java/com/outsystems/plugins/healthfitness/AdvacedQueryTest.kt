76d40ae56a21a2fe5c988ec563ad32727bada31a
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2021-10-13 12:25:04 +0100

    feat: added tests for SimpleQuery feature (e.g. GetHealthData)
    References: https://outsystemsrd.atlassian.net/browse/RMET-1048

--- tests/android/HealthFitnessTests/app/src/test/java/com/outsystems/plugins/healthfitness/AdvacedQueryTest.kt
+++
@@ -1,7 +0,0 @@
-package com.outsystems.plugins.healthfitness
-
-class AdvacedQueryTest {
-
-
-
-}
---
+++ tests/android/HealthFitnessTests/app/src/test/java/com/outsystems/plugins/healthfitness/SimpleQueryTest.kt
@@ -0,0 +1,47 @@
+package com.outsystems.plugins.healthfitness
+
+import com.outsystems.plugins.healthfitness.store.HealthStore
+import org.junit.Assert
+import org.junit.Test
+
+class SimpleQueryTest {
+
+    @Test
+    fun given_InvalidVariable_When_SimpleQuery_Then_VariableNotAvailableError() {
+        val platformInterfaceMock = AndroidPlatformMock()
+        platformInterfaceMock.sendPluginResultCompletion = { result, error ->
+            Assert.assertEquals(result, "null")
+            val code = error?.first
+            val message = error?.second
+            Assert.assertEquals(code, HealthFitnessError.VARIABLE_NOT_AVAILABLE_ERROR.code)
+            Assert.assertEquals(message, HealthFitnessError.VARIABLE_NOT_AVAILABLE_ERROR.message)
+        }
+
+        val googleFitMock = HealthFitnessManagerMock()
+        val store = HealthStore(platformInterfaceMock, googleFitMock)
+
+        store.getLastRecord("Test")
+    }
+
+    @Test
+    fun given_VariableWithoutPermissions_When_SimpleQuery_Then_VariableNotAuthorizedError() {
+        val platformInterfaceMock = AndroidPlatformMock()
+        platformInterfaceMock.sendPluginResultCompletion = { result, error ->
+            Assert.assertEquals(result, "null")
+            val code = error?.first
+            val message = error?.second
+            Assert.assertEquals(code, HealthFitnessError.VARIABLE_NOT_AUTHORIZED_ERROR.code)
+            Assert.assertEquals(message, HealthFitnessError.VARIABLE_NOT_AUTHORIZED_ERROR.message)
+        }
+
+        val googleFitMock = HealthFitnessManagerMock()
+        val store = HealthStore(platformInterfaceMock, googleFitMock)
+
+        googleFitMock.permissionsGranted = false
+
+        store.getLastRecord("HEART_RATE")
+    }
+
+    
+
+}
