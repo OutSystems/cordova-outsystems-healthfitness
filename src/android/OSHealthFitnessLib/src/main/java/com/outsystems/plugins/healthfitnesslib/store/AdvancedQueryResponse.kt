cfb4cb655785d3af1e0283623c63695610c48008
Nelson Lopes Silva <5671236+nflsilva@users.noreply.github.com>
2021-10-22 18:06:17 +0100

    Removed OSHealthFitnessLib code. Added gradle references to .aar in both Plugin and Tests

--- src/android/OSHealthFitnessLib/src/main/java/com/outsystems/plugins/healthfitnesslib/store/AdvancedQueryResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.outsystems.plugins.healthfitnesslib.store
-
-data class AdvancedQueryResponse(
-    val results : List<AdvancedQueryResponseBlock>,
-    var metadata : String? = ""
-)
--- src/android/OSHealthFitnessLib/src/main/java/com/outsystems/plugins/healthfitnesslib/store/GoogleFitPermission.kt
+++
@@ -1,8 +0,0 @@
-package com.outsystems.plugins.healthfitnesslib.store
-
-import com.google.gson.annotations.SerializedName
-
-data class GoogleFitPermission(
-    @SerializedName("Variable") val variable : String,
-    @SerializedName("AccessType") val accessType : String
-)
