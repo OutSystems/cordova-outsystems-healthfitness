b9eac4e741a74dc9c452dd15bc0e8f48965dcc98
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2021-11-10 12:49:49 +0000

    feat: defining test signatures
    References: https://outsystemsrd.atlassian.net/browse/RMET-1192

---
+++ tests/android/HealthFitnessTests/app/src/test/java/com/outsystems/plugins/healthfitness/BackgroundJobTest.kt
@@ -0,0 +1,29 @@
+package com.outsystems.plugins.healthfitness
+
+import org.junit.Test
+
+class BackgroundJobTest {
+
+    @Test
+    fun given_InvalidVariable_When_SettingBackgroundJob_Then_VariableNotAvailableError() {
+        //TODO
+    }
+
+    @Test
+    fun given_VariableWithoutPermissions_When_SettingBackgroundJob_Then_VariableNotAuthorizedError() {
+        //TODO
+    }
+
+    @Test
+    fun given_ValidVariableValidValue_When_SettingBackgroundJob_Then_SomeError() {
+        //TODO
+    }
+
+    @Test
+    fun given_ValidVariableValidValue_When_SettingBackgroundJob_Then_Success() {
+        //TODO
+    }
+
+
+
+}
