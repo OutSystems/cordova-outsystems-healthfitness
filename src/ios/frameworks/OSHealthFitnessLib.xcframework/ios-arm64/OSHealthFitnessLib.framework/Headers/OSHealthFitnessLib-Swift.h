702e778c2759f5fe731991ebb2015eb150bbb1d0
Carlos Corrêa <carlscorrea@gmail.com>
2021-11-12 10:08:34 +0000

    new framework

--- src/ios/frameworks/OSHealthFitnessLib.xcframework/ios-arm64/OSHealthFitnessLib.framework/Headers/OSHealthFitnessLib-Swift.h
+++ src/ios/frameworks/OSHealthFitnessLib.xcframework/ios-arm64/OSHealthFitnessLib.framework/Headers/OSHealthFitnessLib-Swift.h
@@ -218,9 +218,9 @@ SWIFT_CLASS("_TtC18OSHealthFitnessLib13BackgroundJob")
 
 @interface BackgroundJob (SWIFT_EXTENSION(OSHealthFitnessLib))
 @property (nonatomic, copy) NSString * _Nullable comparision;
-@property (nonatomic, copy) NSString * _Nullable frequency;
 @property (nonatomic) int64_t id;
 @property (nonatomic, copy) NSString * _Nullable timeUnit;
+@property (nonatomic) int64_t timeUnitGrouping;
 @property (nonatomic) double value;
 @property (nonatomic, copy) NSString * _Nullable variable;
 @property (nonatomic, strong) Notification * _Nullable notification;
