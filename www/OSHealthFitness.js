b2824b6d24fdd5bedfe5570d7829a31c6f0f3704
Carlos Corrêa <carlscorrea@gmail.com>
2021-12-02 12:06:38 +0000

    feat: background job release preparation

--- www/OSHealthFitness.js
+++ www/OSHealthFitness.js
@@ -36,3 +36,6 @@ exports.getLastRecord = function (success, error, variable) {
     exec(success, error, 'OSHealthFitness', 'getLastRecord', [variable]);
 };
 
+exports.setBackgroundJob = function (success, error, params) {
+    exec(success, error, 'OSHealthFitness', 'setBackgroundJob', [params]);
+};
