da2fe737ec53911bd1da73a248a2a9bfed8a2bd5
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2021-12-13 15:06:00 +0000

    fix: fixing version number

--- src/android/com/outsystems/plugins/healthfitness/oshealthfitnesslibrary/background/database/AppDatabase.kt
+++ src/android/com/outsystems/plugins/healthfitness/oshealthfitnesslibrary/background/database/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.migration.Migration
 
 @Database(
     entities = [BackgroundJob::class, Notification::class],
-    version = 1
+    version = 2
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun backgroundJobDao(): BackgroundJobDao
