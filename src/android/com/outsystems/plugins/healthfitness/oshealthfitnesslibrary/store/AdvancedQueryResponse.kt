3639295757d45192c1cb40d722c718139433ab31
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2022-01-10 15:49:02 +0000

    feat: remove files from library and add library as .aar dependency
    References: https://outsystemsrd.atlassian.net/browse/RMET-1284

--- src/android/com/outsystems/plugins/healthfitness/oshealthfitnesslibrary/store/AdvancedQueryResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.outsystems.plugins.healthfitness.store
-
-import com.outsystems.plugins.healthfitness.HealthFitnessError
-
-data class AdvancedQueryResponse(
-    val results : List<AdvancedQueryResponseBlock>,
-    var metadata : String? = ""
-)
