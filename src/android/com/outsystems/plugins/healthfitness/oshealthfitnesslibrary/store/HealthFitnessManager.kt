63d46c5986f757fa608fd458a31bc410618b29b4
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2022-03-22 16:07:48 +0000

    refactor: comment for future reference

--- src/android/com/outsystems/plugins/healthfitness/oshealthfitnesslibrary/store/HealthFitnessManager.kt
+++ src/android/com/outsystems/plugins/healthfitness/oshealthfitnesslibrary/store/HealthFitnessManager.kt
@@ -195,6 +195,7 @@ class HealthFitnessManager(var context : Context, var activity : Activity? = nul
 
     private fun getSubscritionPendingIntent(variableName : String) : PendingIntent {
         //do the actual subscription to the variable updates
+        //when MABS 7 stops being supported, we can use PendingIntent.FLAG_MUTABLE instead of 33554432
         val intent = Intent(context, VariableUpdateService::class.java)
         intent.putExtra(VariableUpdateService.VARIABLE_NAME, variableName)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
