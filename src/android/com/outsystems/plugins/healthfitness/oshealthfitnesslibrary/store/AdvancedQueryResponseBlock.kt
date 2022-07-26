44dbec0ed181ad4f37d18a4a3fe8e691c7b8e084
Nelson Lopes Silva <5671236+nflsilva@users.noreply.github.com>
2022-07-26 14:42:22 +0100

    RMET-1480 Use of already built libraries (#75)
    * removed tests and code. added lib dependencies
    
    * fix: changed lib version to match the future of lib

--- src/android/com/outsystems/plugins/healthfitness/oshealthfitnesslibrary/store/AdvancedQueryResponseBlock.kt
+++
@@ -1,8 +0,0 @@
-package com.outsystems.plugins.healthfitness.store
-
-data class AdvancedQueryResponseBlock (
-    val block : Int,
-    val startDate : Long,
-    val endDate : Long,
-    val values : MutableList<Float>
-)
--- src/android/com/outsystems/plugins/healthfitness/oshealthfitnesslibrary/store/HealthStoreException.kt
+++
@@ -1,8 +0,0 @@
-package com.outsystems.plugins.healthfitness.store
-
-import com.outsystems.plugins.healthfitness.HealthFitnessError
-import java.lang.Exception
-
-class HealthStoreException(val error : HealthFitnessError) : Exception() {
-
-}
