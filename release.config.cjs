d3c06dfc4b813439f69dc79e1426576f59c3fd28
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-10-23 14:15:59 +0100

    ci: Remove temporary branch trigger

--- release.config.cjs
+++ release.config.cjs
@@ -4,8 +4,7 @@ const xml2js = require('xml2js');
 module.exports = {
   branches: [ // releases done in "release" branch, PR opened to main
     { name: 'main', prerelease: false },
-    { name: 'release', prerelease: false },
-    { name: 'test/automated-release', prerelease: false }
+    { name: 'release', prerelease: false }
   ],
   // releases done in "release" branch, PR opened to main
   tagFormat: '${version}',  // semantic-release uses vX.Y.Z by default, but our cordova plugins expect X.Y.Z
